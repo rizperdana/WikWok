2ee95a6c248b1cd8dacd0bc1f53eef3d7dd1a357
Rizki Perdana <perdana.rizki16@gmail.com>
2026-01-25 23:10:11 +0700

    adjust script inclusion

--- src/components/GoogleAdSense.tsx
+++ src/components/GoogleAdSense.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import Script from 'next/script';
 
 export function GoogleAdSense() {
   const pId = process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID;
   if (!pId) return null;
 
   return (
-    <Script
+    <script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId.replace('pub-', '')}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
     />
   );
 }
